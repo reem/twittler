937a21ccb2af44e17804944323a4bbba580d56e3
Jonathan Reem <jonathan.reem@gmail.com>
2014-03-28 22:41:58 -0400

    Reformatted.

--- data_generator.js
+++ data_generator.js
@@ -15,15 +15,15 @@ window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
-  var username = newTweet.user;
-  streams.users[username].push(newTweet);
-  streams.home.push(newTweet);
+    var username = newTweet.user;
+    streams.users[username].push(newTweet);
+    streams.home.push(newTweet);
 };
 
 // utility function
 var randomElement = function(array){
-  var randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 };
 
 // random tweet generator
@@ -34,36 +34,36 @@ var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'mon
 var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
 var randomMessage = function(){
-  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+    return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
 var generateRandomTweet = function(){
-  var tweet = {};
-  tweet.user = randomElement(users);
-  tweet.message = randomMessage();
-  tweet.created_at = new Date();
-  addTweet(tweet);
+    var tweet = {};
+    tweet.user = randomElement(users);
+    tweet.message = randomMessage();
+    tweet.created_at = new Date();
+    addTweet(tweet);
 };
 
 for(var i = 0; i < 10; i++){
-  generateRandomTweet();
+    generateRandomTweet();
 }
 
 var scheduleNextTweet = function(){
-  generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+    generateRandomTweet();
+    setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
 scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
 var writeTweet = function(message){
-  if(!visitor){
-    throw new Error('set the global visitor property!');
-  }
-  var tweet = {};
-  tweet.user = visitor;
-  tweet.message = message;
-  addTweet(tweet);
+    if(!visitor){
+        throw new Error('set the global visitor property!');
+    }
+    var tweet = {};
+    tweet.user = visitor;
+    tweet.message = message;
+    addTweet(tweet);
 };
